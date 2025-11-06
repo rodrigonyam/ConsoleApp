d5a32fd4f786532f039b0dcaf4c1b7012e6d3af2
Rodrigue Nyamwoga <rodrigonyam@gmail.com>
2025-11-06 14:04:19 -0500

    new commit

--- csharpcode/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ csharpcode/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccf17fc9a307d999faf4432ae9c3cf236d88d5c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+942d7050ec37fbe1a28048d58b8fd1db6013946e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
